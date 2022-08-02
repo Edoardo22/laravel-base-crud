2ddfb0dd4ae779bbf2daa987b0847622859bcb17
MyUsername <vanaliedoardo@gmail.com>
2022-08-02 18:47:47 +0200

    es finito

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,6 +39,7 @@ class ComicController extends Controller
         $comic = new Comic;
         $comic->title = $request->title;
         $comic->description = $request->description;
+        $comic->thumb = $request->thumb;
         $comic->price = $request->price;
         $comic->series = $request->series;
         $comic->sale_date = $request->sale_date;
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,46 +6,37 @@
     @csrf
 
     <div class="mb-3">
-      <label for="titleInput" class="form-label">title</label>
-      <input type="text" class="form-control" name="title" id="titleInput">
+        <label for="titleComic" class="form-label">Titolo Fumetto</label>
+        <input type="title" name="title" class="form-control" id="titleComic" placeholder="Titolo Fumetto">
     </div>
-
     <div class="mb-3">
-      <label for="textInput" class="form-label">description</label>
-      <textarea class="form-control" name="text" id="textInput" rows="3"></textarea>
+        <label for="descriptionComic" class="form-label">Descrizione Fumetto</label>
+        <textarea class="form-control" name="description" id="descriptionComic" rows="3"></textarea>
     </div>
-
     <div class="mb-3">
-        <label for="textInput" class="form-label">thumb</label>
-        <textarea class="form-control" name="text" id="textInput" rows="1"></textarea>
-      </div>
-
-      <div class="mb-3">
-        <label for="textInput" class="form-label">price</label>
-        <textarea class="form-control" name="textInput" id="textInput" rows="1"></textarea>
-      </div>
-
-      <div class="mb-3">
-        <label for="textInput" class="form-label">series</label>
-        <textarea class="form-control" name="text" id="textInput" rows="3"></textarea>
-      </div>
-
-      <div class="mb-3">
-        <label for="textInput" class="form-label">sale_date</label>
-        <textarea class="form-control" name="text" id="textInput" rows="3"></textarea>
-      </div>
-
-      <div class="form-floating mb-3">
-        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-          <option selected>comic book</option>
-          <option value="1">graphic novel</option>
-        </select>
-        <label for="floatingSelect">comic type</label>
-      </div> 
+        <label for="imageSrc" class="form-label">Percorso Immagine</label>
+        <input type="title" name="thumb" class="form-control" id="imageSrc" placeholder="Percorso Immagine">
+    </div>
+    <div class="mb-3">
+        <label for="priceComic" class="form-label">Prezzo Fumetto</label>
+        <input type="title" name="price" class="form-control" id="priceComic" placeholder="Prezzo Fumetto">
+    </div>
+    <div class="mb-3">
+        <label for="seriesComic" class="form-label">Serie Fumetto</label>
+        <input type="title" name="series" class="form-control" id="seriesComic" placeholder="Serie Fumetto">
+    </div>
+    <div class="mb-3">
+        <label for="dateComic" class="form-label">Data uscita Fumetto</label>
+        <input type="title" name="sale_date" class="form-control" id="dateComic" placeholder="Data uscita Fumetto">
+    </div>
+    <div class="mb-3">
+        <label for="typeComic" class="form-label">Tipo Fumetto</label>
+        <input type="title" name="type" class="form-control" id="typeComic" placeholder="Tipo Fumetto">
+    </div>
 
     <div class="d-flex gap-2">
-      <button type="submit" class="btn btn-success">Salva</button>
-      <a href="{{ route('comics.index') }}" class="btn btn-secondary">Annulla</a>
+        <button type="submit" class="btn btn-success">Aggiungi</button>
+        <a href="{{ route("comics.index")}}" class="btn btn-secondary">Annulla</a>
     </div>
   </form>
 @endsection
